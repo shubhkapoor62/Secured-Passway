298a7bcb2958dba4d3114b0f73002f6b661df617
shubhkapoor62 <shubhkapoor62@gmail.com>
2017-06-14 16:00:13 +0530

    user added

---
+++ src/user.java
@@ -0,0 +1,35 @@
+
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.QVariant;
+
+class user extends QMainWindow
+{
+    public user()
+    {
+        QMenuBar m = new QMenuBar();
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String args[])
+    {
+        QApplication.initialize(args);
+        QApplication.execStatic();
+        
+    }
+    
+}
